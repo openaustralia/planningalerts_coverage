d78e8307745241d8fddb7ae0e3e44259cbd11564
Luke Bacon <luke@equivalentideas.com>
2016-05-27 15:53:37 +1000

    Add basic scraper code to get the numbers we want to track

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,24 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'scraperwiki'
+require 'mechanize'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+agent = Mechanize.new
+
+# Read in a page
+page = agent.get("https://www.planningalerts.org.au/authorities/")
+
+sentence = page.at("#content").search("p")[1].text
+words = sentence.split(" ")
+
+record = {
+  date_scraped: Date.today.to_s,
+  percentage_of_population_convered: words[3].delete("%").to_i,
+  number_of_authorities: words[8].to_i
+}
+
+p record
+
+# # Write out to the sqlite database using scraperwiki library
+ScraperWiki.save_sqlite([:date_scraped], record)
