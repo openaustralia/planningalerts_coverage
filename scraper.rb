7201d616217707b4f60648efe8862af75f2a0d9b
Luke Bacon <luke@equivalentideas.com>
2016-05-27 16:03:30 +1000

    Break if the wording has changed

--- scraper.rb
+++ scraper.rb
@@ -12,13 +12,17 @@ page = agent.get("https://www.planningalerts.org.au/authorities/")
 sentence = page.at("#content").search("p")[1].text
 words = sentence.split(" ")
 
-record = {
-  date_scraped: Date.today.to_s,
-  percentage_of_population_convered: words[3].delete("%").to_i,
-  number_of_authorities: words[8].to_i
-}
+if words.count.eql? 54
+  record = {
+    date_scraped: Date.today.to_s,
+    percentage_of_population_convered: words[3].delete("%").to_i,
+    number_of_authorities: words[8].to_i
+  }
 
-p record
+  p record
 
-# # Write out to the sqlite database using scraperwiki library
-ScraperWiki.save_sqlite([:date_scraped], record)
+  # # Write out to the sqlite database using scraperwiki library
+  ScraperWiki.save_sqlite([:date_scraped], record)
+else
+  raise "Paragraph has changed"
+end
